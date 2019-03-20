ea97cc48b3a88ec25f9e3ed13f937953d012ed39
AustinBCole <austin.cole.chileno@gmail.com>
2019-03-20 16:38:50 -0600

    Add signup views, hook up signup views.

---
+++ BirthRide/BirthRide/Empty.swift
@@ -0,0 +1,16 @@
+//
+//  CustomSegue.swift
+//  BirthRide
+//
+//  Created by Austin Cole on 3/20/19.
+//  Copyright © 2019 Austin Cole. All rights reserved.
+//
+
+import UIKit
+
+class Empty: UIStoryboardSegue{
+    
+    override func perform() {
+        
+    }
+}
