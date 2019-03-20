8b114d89c71d3ed0d9cf9f204d9e287d1dbe0322
Sean Hendrix <hendoware@gmail.com>
2019-03-20 18:26:46 -0400

    redo model, add enum, welcome view

---
+++ BirthRide/BirthRide/View Controllers/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  BirthRide
+//
+//  Created by Sean Hendrix on 3/20/19.
+//  Copyright © 2019 Austin Cole. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+}
