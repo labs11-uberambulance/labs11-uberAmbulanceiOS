4641497a7393effef3b92452cafc820bc5411c0a
AustinBCole <austin.cole.chileno@gmail.com>
2019-03-21 15:26:21 -0600

    Setup ConfirmRideView as a xib. Hook up outlets.

---
+++ BirthRide/BirthRide/View Controllers/ConfirmRideViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ConfirmRideViewController.swift
+//  BirthRide
+//
+//  Created by Austin Cole on 3/21/19.
+//  Copyright © 2019 Austin Cole. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ConfirmRideViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
