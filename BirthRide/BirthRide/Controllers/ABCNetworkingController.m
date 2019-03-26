f6031bae655274f2fdb3d18169bacfac709d69f6
AustinBCole <austin.cole.chileno@gmail.com>
2019-03-26 17:53:25 -0600

    Continue to implement OAuth in the EmailAuthorizationViewController. Continue fixing bug causing certain nibs to not load correctly.

--- BirthRide/BirthRide/Controllers/ABCNetworkingController.m
+++ BirthRide/BirthRide/Controllers/ABCNetworkingController.m
@@ -1,41 +1,41 @@
+////
+////  NetworkingController.m
+////  BirthRide
+////
+////  Created by Austin Cole on 3/26/19.
+////  Copyright © 2019 Austin Cole. All rights reserved.
+////
 //
-//  NetworkingController.m
-//  BirthRide
+//#import "ABCNetworkingController.h"
 //
-//  Created by Austin Cole on 3/26/19.
-//  Copyright © 2019 Austin Cole. All rights reserved.
+//@implementation ABCNetworkingController
 //
-
-#import "ABCNetworkingController.h"
-
-@implementation NetworkingController
-
-- (void)fetchMotherWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-- (void)fetchNearbyDriversWithLatitude:(double)latitude withLongitude:(double)longitude withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-- (void)fetchRideWithToken:(NSString *)token withCompletion:(Ride * _Nonnull (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-- (void)authenticateUserWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-- (void)createRideWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-- (void)updateRideWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-- (void)updateUserWithToken:(NSString *)token userType:(NSString *)userType withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
-    
-}
-
-@end
+//- (void)fetchMotherWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//- (void)fetchNearbyDriversWithLatitude:(double)latitude withLongitude:(double)longitude withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//- (void)fetchRideWithToken:(NSString *)token withCompletion:(Ride * _Nonnull (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//- (void)authenticateUserWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//- (void)createRideWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//- (void)updateRideWithToken:(NSString *)token withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//- (void)updateUserWithToken:(NSString *)token userType:(NSString *)userType withCompletion:(void (^)(NSError * _Nonnull))completionHandler {
+//    
+//}
+//
+//@end
